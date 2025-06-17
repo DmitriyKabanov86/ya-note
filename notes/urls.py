af5020c59c827224cbe9636630d18bfb987bd2c8
Дмитрий Кабанов <ko6a86@yandex.ru>
2025-06-17 22:38:10 +0300

    tests_test_routes_done

--- notes/urls.py
+++ notes/urls.py
@@ -5,11 +5,11 @@ from notes import views
 app_name = 'notes'
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
-    path('add/', views.NoteCreate.as_view(), name='add'),
-    path('edit/<slug:slug>/', views.NoteUpdate.as_view(), name='edit'),
-    path('note/<slug:slug>/', views.NoteDetail.as_view(), name='detail'),
-    path('delete/<slug:slug>/', views.NoteDelete.as_view(), name='delete'),
-    path('notes/', views.NotesList.as_view(), name='list'),
-    path('done/', views.NoteSuccess.as_view(), name='success'),
+    path('', views.Home.as_view(), name='home'),  # Должна быть доступна анонимному пользователю.
+    path('add/', views.NoteCreate.as_view(), name='add'),  # Доступна аут. пользователю. Аноим редирект на логин.
+    path('edit/<slug:slug>/', views.NoteUpdate.as_view(), name='edit'),  # Доступна автору. Аноим редирект на логин.
+    path('note/<slug:slug>/', views.NoteDetail.as_view(), name='detail'),  # Доступна автору. Аноим редирект на логин.
+    path('delete/<slug:slug>/', views.NoteDelete.as_view(), name='delete'),  # Доступна автору. Аноим редирект на логин.
+    path('notes/', views.NotesList.as_view(), name='list'),  # Доступна аут. пользователю. Аноим редирект на логин.
+    path('done/', views.NoteSuccess.as_view(), name='success'),  # Доступна аут. пользователю. Аноим редирект на логин.
 ]
